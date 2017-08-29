3e9103a895bb6392cd6b229df234f2f65d971a6a
manueldeveloper <manueldeveloper@gmail.com>
2017-08-29 12:40:41 +0200

    Enable "auto-discovery" eureka service

--- SimpleRestAPI/src/main/java/com/example/demo/SimpleRestApiApplication.java
+++ SimpleRestAPI/src/main/java/com/example/demo/SimpleRestApiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,6 +21,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SimpleRestApiApplication {
 
