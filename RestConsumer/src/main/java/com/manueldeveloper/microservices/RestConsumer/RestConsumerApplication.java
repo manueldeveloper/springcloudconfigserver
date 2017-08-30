3da3dce9ff6f6249fbbeb16e3138f4184756a895
manueldeveloper <manueldeveloper@gmail.com>
2017-08-30 17:51:11 +0200

    Adding Hystrix fallback method

--- RestConsumer/src/main/java/com/manueldeveloper/microservices/RestConsumer/RestConsumerApplication.java
+++ RestConsumer/src/main/java/com/manueldeveloper/microservices/RestConsumer/RestConsumerApplication.java
@@ -1,11 +1,13 @@
 package com.manueldeveloper.microservices.RestConsumer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@EnableCircuitBreaker
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -35,6 +40,11 @@ class ReservationApiGatewayRestController {
 	@Autowired
 	private RestTemplate request;
 	
+	public Collection<String> getReservationNamesFallback(){
+		return new ArrayList<>();
+	}
+	
+	@HystrixCommand(fallbackMethod = "getReservationNamesFallback")
 	@RequestMapping("/names")
 	public Collection<String> getReservationNames(){
 		
